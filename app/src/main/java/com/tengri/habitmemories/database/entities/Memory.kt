eae04634ca7b67431417091fe7e327ef0b481b06
Idris Bulut <idrisbulut00@gmail.com>
2021-05-01 02:27:04 +0300

    foreign key eklendi

--- app/src/main/java/com/tengri/habitmemories/database/entities/Memory.kt
+++ app/src/main/java/com/tengri/habitmemories/database/entities/Memory.kt
@@ -1,10 +1,18 @@
 package com.tengri.habitmemories.database.entities
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(
+    foreignKeys = [ForeignKey(
+        entity = Habit::class,
+        parentColumns = ["id"],
+        childColumns = ["habitId"],
+        onDelete = CASCADE
+    )]
+)
 data class Memory(
     @PrimaryKey(autoGenerate = true) var id: Long,
     val habitId: Long,
