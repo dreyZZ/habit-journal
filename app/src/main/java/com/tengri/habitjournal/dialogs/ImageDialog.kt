4142088bd0d19c6bc987423457799d20ccb9936e
Idris Bulut <idrisbulut00@gmail.com>
2021-06-14 22:35:48 +0300

    experience image changes

--- app/src/main/java/com/tengri/habitjournal/dialogs/ImageDialog.kt
+++ app/src/main/java/com/tengri/habitjournal/dialogs/ImageDialog.kt
@@ -2,16 +2,19 @@ package com.tengri.habitjournal.dialogs
 
 import android.app.Dialog
 import android.content.Context
+import android.content.res.Configuration
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.ImageButton
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.github.chrisbanes.photoview.PhotoView
 import com.tengri.habitjournal.R
+import com.tengri.habitjournal.util.convertByteArrayToBmp
 
 class ImageDialog(
     context: Context,
@@ -28,8 +31,6 @@ class ImageDialog(
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_experience_image)
 
-        window!!.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-
         val photoView = findViewById<PhotoView>(R.id.imageView)
         val imageButton = findViewById<ImageButton>(R.id.imageButton)
 
@@ -37,8 +38,36 @@ class ImageDialog(
             .load(imageBytes)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .transition(DrawableTransitionOptions.withCrossFade())
-            .placeholder(ColorDrawable(Color.BLACK))
+            .placeholder(ColorDrawable(Color.TRANSPARENT))
             .into(photoView)
+
+//        val bmp = convertByteArrayToBmp(imageBytes)
+//        photoView.setImageBitmap(bmp)
+
+        when (context.resources.configuration.orientation) {
+            Configuration.ORIENTATION_LANDSCAPE -> {
+                window!!.setLayout(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+
+                photoView.layoutParams = ConstraintLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            }
+            else -> {
+                window!!.setLayout(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+
+                photoView.layoutParams = ConstraintLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+            }
+        }
 
         imageButton.setOnClickListener {
             dismiss()
